29d05eb31e5778a6f9affa7f76ca66ca220f5087
DhanviDesai <dhanvidesai99@gmail.com>
2019-11-18 20:14:32 +0530

    First Commit

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'localhost:3036');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', '');
+   define('DB_DATABASE', 'ark');
+   $db = mysqli_connect('localhost',DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
